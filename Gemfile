118bfc52f28ac327acb394e6c0ec95ca6f239bfd
skidder <skidder@brightcove.com>
2014-02-26 09:26:21 -0800

    Added uuid gem dependency to allow use with Ruby 1.8.7

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "http://rubygems.org"
 
+gem "uuid"
 gem "rake"
 
 gemspec
--- lib/queue_classic/worker.rb
+++ lib/queue_classic/worker.rb
@@ -93,7 +93,7 @@ module QC
     end
 
     def start_heartbeat(job)
-      wid = SecureRandom.uuid
+      wid = UUID.uuid4
       QC.log_yield(:at => "start_heartbeat", :jid => job[:id], :wid => wid) do
         @heartbeat = Thread.new do
           loop do
